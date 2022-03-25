f6c7101ce49639ee950cc61ca04e1ee14dd17e0f
Sergey13111 <boiko.sergey93@gmail.com>
2022-03-25 14:58:42 +0200

    added HW-12_Fetch

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((response) => response.json())
+// .then((data) => console.log(data))
+// .catch((error) => console.error(error))
+
+const getUsers = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        console.log("data", data);
+        const user = data.filter(user => (user.company.name === 'Johns Group'));
+        return console.log(user);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        // console.log("Finished!");
+    }
+};
+
+getUsers();
